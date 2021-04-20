6cf6b1ce792f1db4ef17fee5c9f2ee560a472e26
Paint19 <hanna@schaff.se>
2021-04-20 16:24:01 +0200

    check shell

--- projektfiler/CommandAddSection.ts
+++ projektfiler/CommandAddSection.ts
@@ -2,6 +2,20 @@ import Discord from 'discord.js';
 
 export class CommandAddSection {
     doIt(message : Discord.Message, args){
+
+        //check message sender role
+        if(false) //if not mod (replace false with test access-method later)
+        {
+            message.channel.send('You are not a moderator');
+            return;
+        }
+        else //if it is a mod
+        {
+            console.log(args);
+        }
+
+
+
         //message.channel.send();
     }
 }
