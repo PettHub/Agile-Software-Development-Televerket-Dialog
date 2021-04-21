3f30f37ff4655537e9e106d910697b99b7c37653
Markus <markus-grahn@hotmail.com>
2021-04-21 13:59:51 +0200

    Listener will now terminate

--- projektfiler/Pms.ts
+++ projektfiler/Pms.ts
@@ -5,14 +5,8 @@ export class PMHandler {
         
     }
     doIt(author : Discord.User, client : Discord.Client, guild : Discord.Guild){
-        author.send('Please send 3 images, you have 5 minutes');
-        let timeout = setTimeout(function(){author.send('Timed out, please')}, 1000*60*5)
-        let messagesRemaining = 3;
-        let repostChannel;
-        repostChannel = client.channels.cache.get('826895001446645800');
-        let images : string[];
-        images = [null, null, null];
-        client.on('message', message =>{
+
+        let listener = message => {
             if (author === message.author){
                 var Attachment = message.attachments.array();
                 Attachment.forEach(function(attachment){
@@ -24,9 +18,22 @@ export class PMHandler {
                             repostChannel.send(image + ' user: ' + author.id);
                         })
                         author.send('Your application has been sent, you will get a response within 24 hours.');
+                        client.removeListener('message', listener);
                     }
                 })
             }
-        });
+        }
+
+        author.send('Please send 3 images, you have 5 minutes');
+        let timeout = setTimeout(function(){author.send('Timed out, please'); client.removeListener('message', listener);}, 1000*60*5);
+        let messagesRemaining: number = 3;
+        let repostChannel;
+        repostChannel = client.channels.cache.get('826895001446645800');
+        let images : string[];
+        images = [null, null, null];
+        client.on('message', listener);
+
     }
+    
+
 }
