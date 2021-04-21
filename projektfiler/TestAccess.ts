ee8d96819b4dcd266b3717a81b5c21bf3e0a1d61
Mattias <mattias.hult@hotmail.com>
2021-04-21 14:31:50 +0200

    Utveckling av TestAccess

--- projektfiler/TestAccess.ts
+++ projektfiler/TestAccess.ts
@@ -1,7 +1,31 @@
 import Discord from 'discord.js';
 
 export class TestAccess {
-    doIt(message : Discord.Message, args){
-        
+    
+
+    doIt(message : Discord.Message, args : string){
+        let modlist : Array<string>;        
+    
+       // if (args.){
+         //   if (modlist.contains(author)){
+           //     return true;
+            //}
+        //else {
+          //  return false;
+        //}
+        //}
+
+        const command = args.shift().toLowerCase();
+
+        switch (command){
+            case 'mod':
+                if(modlist.contains());
+                break;
+            case 'cool':
+                message.channel.send('me');
+                break;
+        }
+
+
     }
 }
