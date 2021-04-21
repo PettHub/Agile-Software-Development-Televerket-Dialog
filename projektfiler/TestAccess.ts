363fc5533d40beb61cdaf8b2000d71c79994b9f2
Mattias <mattias.hult@hotmail.com>
2021-04-21 15:36:35 +0200

    Uppstädning av kod, fungerande owner/mod

--- projektfiler/TestAccess.ts
+++ projektfiler/TestAccess.ts
@@ -1,31 +1,48 @@
 import Discord from 'discord.js';
 
 export class TestAccess {
+    mod : string;
+
+    constructor(role : string){
+        this.mod = role;
+    }
+
+    doIt(message : Discord.Message, accessLevel : string){
+       // let modlist : Array<string>;        
     
-
-    doIt(message : Discord.Message, args : string){
-        let modlist : Array<string>;        
-    
-       // if (args.){
-         //   if (modlist.contains(author)){
-           //     return true;
-            //}
-        //else {
-          //  return false;
-        //}
-        //}
-
-        const command = args.shift().toLowerCase();
-
-        switch (command){
-            case 'mod':
-                if(modlist.contains());
-                break;
-            case 'cool':
-                message.channel.send('me');
-                break;
+       switch (accessLevel){
+        case 'mod':
+            if (message.author.id === message.member.guild.ownerID || message.author.id === this.mod){
+                return true;
+            }
+            else{
+                return false;
+            }
+            break;
+        case 'owner':
+            if (message.author.id === message.member.guild.ownerID){
+                return true;
+            }
+            else{
+                return false;
+            }
+            break;
+        case 'member':
+            return true;
+            break;
+       
         }
+        return false;
+    }
 
-
+    setMod(message : Discord.Message, command){
+        console.log(command);
+        if (this.doIt(message, 'owner')){
+            this.mod = command;
+            message.channel.send('OK');
+        }
+        else{
+            message.channel.send('Must be owner');
+        }
     }
 }
--- projektfiler/main.ts
+++ projektfiler/main.ts
@@ -1,4 +1,5 @@
 import {CommandPing} from './CommandPing';
+import {TestAccess} from './TestAccess';
 import Discord from 'discord.js';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -13,6 +14,7 @@ let prefix = process.env.DISCORD_PREFIX;
 client.once('ready', () => {
     console.log('bot is now online');
 });
+let accesscontrol = new TestAccess('');
 
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
@@ -27,6 +29,14 @@ client.on('message', message =>{
         case 'cool':
             message.channel.send('me');
             break;
+        case 'hasaccess':
+            accesscontrol.doIt(message, 'mod');
+            break;
+        case 'setmod':
+            accesscontrol.setMod(message, args.shift().toLowerCase());
+            break;
+
+
     }
 });
 
