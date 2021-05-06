3a47c111456ec3c0a629876284312e04801188e0
Christian <clind0429@gmail.com>
2021-05-06 18:03:33 +0200

    Nominator connected to database, should be tested

--- projektfiler/DatabaseFunctions.ts
+++ projektfiler/DatabaseFunctions.ts
@@ -43,9 +43,8 @@ export class DatabaseFunctions {
         ); //Creates a table for Users
 
         this.db.run(
-            "CREATE TABLE IF NOT EXISTS Nominations(user CHAR(20) NOT NULL, section TEXT NOT NULL, FOREIGN KEY(user) REFERENCES Users(user) ON DELETE CASCADE, FOREIGN KEY(section) REFERENCES Sections(section) ON DELETE CASCADE,PRIMARY KEY(user, section))"
+            "CREATE TABLE IF NOT EXISTS Nominations(nominator CHAR(20) NOT NULL, user CHAR(20) NOT NULL, section TEXT NOT NULL, stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(user) REFERENCES Users(user) ON DELETE CASCADE, FOREIGN KEY(section) REFERENCES Sections(section) ON DELETE CASCADE,PRIMARY KEY(user, section))"
         ); //Creates a table for Nominations
-
         this.db.run(
             "CREATE TABLE IF NOT EXISTS Votes(id INTEGER, stamp TIMESTAMP NOT NULL, voter CHAR(20) NOT NULL, votee CHAR(20) NOT NULL, section TEXT NOT NULL, FOREIGN KEY (votee, section) REFERENCES Nominations(user, section) ON DELETE CASCADE, FOREIGN KEY(voter) REFERENCES Users(user) ON DELETE CASCADE,PRIMARY KEY(id AUTOINCREMENT))"
         ); //Creates a table for Votes
